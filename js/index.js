e7c3051b2f59d2db2fde1a24dbde13ef803ff582
Francisco Rios <pacoriosgalan@gmail.com>
2021-02-27 12:53:09 +0100

    Change loader id name.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ import SpinnerController from './controllers/SpinnerController.js';
 // TODO: Pasar los nombres de los eventos a variables globales
 
 window.addEventListener('DOMContentLoaded', async (event) => {
-  const spinner = document.querySelector('.lds-ring');
+  const spinner = document.querySelector('.spinner-loader');
   const spinnerController = new SpinnerController(spinner);
 
   const element = document.querySelector('.items-list');
