7f41b35fbcdda1adbe8eeb980f015b1557e47400
Francisco Rios <pacoriosgalan@gmail.com>
2021-02-27 16:19:21 +0100

    Add proper UI when DB doesn't have items to show

---
+++ js/views/noItemsView.js
@@ -0,0 +1,8 @@
+export const noItemsView = () => {  
+  return `<div class="container">
+    <div class="notification is-primary">
+      <p>ESTA ES LA NO ITEMS VIEW que refleja fielmente mi creatividad.</p>
+      <p>Why not create a new ad? ... button bla bla bla...</p>
+    </div>
+  </div>`
+};
